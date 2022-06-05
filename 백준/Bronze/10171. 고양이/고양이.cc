64c73bc57fb1013512e573a8b3a54febb94a06b1
pushrsp <pushrsp@gmail.com>
2022-06-05 13:19:32 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a = "\\    /\\\n"
+               " )  ( ')\n"
+               "(  /  )\n"
+               " \\(__)|";
+
+    cout << a << endl;
+    return 0;
+}
