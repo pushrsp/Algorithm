4bf616e2540dfd6017244694107c3b3fb785a925
pushrsp <pushrsp@gmail.com>
2022-09-20 21:43:19 +0900

    [level 3] Title: 양과 늑대, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.cpp
@@ -2,57 +2,51 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int answer = 0;
-vector<vector<int>> TREE;
-vector<int> INFO;
+vector<vector<int>> Tree;
+vector<int> Info;
+vector<bool> Visited(131073, false);
 
-bool visited[18][18][18];
+void dfs(int sheep, int wolf, int idx, int visited) {
+    if (Info[idx])
+        wolf++;
+    else
+        sheep++;
 
-void DFS(int idx, int sheep, int wolf) {
     answer = max(answer, sheep);
+    if (wolf >= sheep)
+        return;
 
-    for (int next: TREE[idx]) {
-        if (INFO[next] == 0) {
-            if (!visited[next][sheep + 1][wolf]) {
-                visited[next][sheep + 1][wolf] = true;
-                INFO[next] = -1;
-                DFS(next, sheep + 1, wolf);
-                INFO[next] = 0;
-                visited[next][sheep + 1][wolf] = false;
-            }
-        } else if (INFO[next] == 1) {
-            if (!visited[next][sheep][wolf + 1] && sheep > wolf + 1) {
-                visited[next][sheep][wolf + 1] = true;
-                INFO[next] = -1;
-                DFS(next, sheep, wolf + 1);
-                INFO[next] = 1;
-                visited[next][sheep][wolf + 1] = false;
-            }
-        } else {
-            if (!visited[next][sheep][wolf]) {
-                visited[next][sheep][wolf] = true;
-                DFS(next, sheep, wolf);
-                visited[next][sheep][wolf] = false;
-            }
+    for (int i = 0; i < Info.size(); ++i) {
+        if (!(visited & (1 << i)))
+            continue;
+
+        for (auto &child: Tree[i]) {
+            int nextVisited = visited | (1 << child);
+
+            if (Visited[nextVisited])
+                continue;
+
+            Visited[nextVisited] = true;
+            dfs(sheep, wolf, child, nextVisited);
         }
     }
 }
 
 int solution(vector<int> info, vector<vector<int>> edges) {
-    INFO = info;
-    TREE = vector<vector<int>>(info.size());
-
-    for (vector<int> &edge: edges) {
-        TREE[edge[0]].push_back(edge[1]);
-        TREE[edge[1]].push_back(edge[0]);
+    Info = info;
+    Tree = vector<vector<int>>(info.size());
+    for (auto &edge: edges) {
+        Tree[edge[0]].push_back(edge[1]);
+        Tree[edge[1]].push_back(edge[0]);
     }
 
-    INFO[0] = -1;
-    visited[0][1][0] = true;
-    DFS(0, 1, 0);
+    Visited[1 << 0] = true;
+    dfs(0, 0, 0, (1 << 0));
 
     return answer;
 }
