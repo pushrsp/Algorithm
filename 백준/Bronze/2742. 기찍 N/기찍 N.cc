a28e0e7b5adb6fb72c23b26441a953467ae0765d
pushrsp <pushrsp@gmail.com>
2022-09-23 09:49:37 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+	cin >> N;
+
+	for (int i = N; i > 0; i--)
+		cout << i << '\n';
+	
+	return 0;
+}
