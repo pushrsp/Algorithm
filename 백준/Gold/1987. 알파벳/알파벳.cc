507e5b5e7df9de7a77e0cffa46f80dd091f0ceac
pushrsp <pushrsp@gmail.com>
2022-09-29 14:21:27 +0900

    [Gold IV] Title: 알파벳, Time: 404 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,50 +1,56 @@
-#include <iostream>
+#include <string>
+#include <vector>
 #include <queue>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int R, C, Answer = -1;
 vector<vector<char>> Map;
-bool Visited[26];
 
-int deltaY[4] = {-1, 0, 1, 0};
-int deltaX[4] = {0, -1, 0, 1};
+int deltaY[4] = { -1,0,1,0 };
+int deltaX[4] = { 0,-1,0,1 };
 
-void dfs(int nowY, int nowX, int cnt) {
-    Answer = max(Answer, cnt);
-    for (int i = 0; i < 4; ++i) {
-        int nextY = nowY + deltaY[i];
-        int nextX = nowX + deltaX[i];
+void go(int cnt, int visited, int nowY, int nowX)
+{
+	Answer = max(Answer, cnt);
+	for (int i = 0; i < 4; i++)
+	{
+		int nextY = nowY + deltaY[i];
+		int nextX = nowX + deltaX[i];
 
-        if (nextY < 0 || nextY >= R)
-            continue;
-        if (nextX < 0 || nextX >= C)
-            continue;
-        if (Visited[Map[nextY][nextX] - 'A'])
-            continue;
+		if (nextY < 0 || nextY >= R)
+			continue;
+		if (nextX < 0 || nextX >= C)
+			continue;
+		if (visited & (1 << (Map[nextY][nextX] - 'A')))
+			continue;
 
-        Visited[Map[nextY][nextX] - 'A'] = true;
-        dfs(nextY, nextX, cnt + 1);
-        Visited[Map[nextY][nextX] - 'A'] = false;
-    }
+		int nextVisited = visited | (1 << (Map[nextY][nextX] - 'A'));
+		go(cnt + 1, nextVisited, nextY, nextX);
+		
+	}
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> R >> C;
+	cin >> R >> C;
 
-    Map = vector<vector<char>>(R, vector<char>(C));
-    for (int y = 0; y < R; ++y) {
-        for (int x = 0; x < C; ++x) {
-            cin >> Map[y][x];
-        }
-    }
+	Map = vector<vector<char>>(R, vector<char>(C));
+	for (int y = 0; y < R; y++)
+	{
+		for (int x = 0; x < C; x++)
+		{
+			cin >> Map[y][x];
+		}
+	}
 
-    Visited[Map[0][0] - 'A'] = true;
-    dfs(0, 0, 1);
-    cout << Answer << '\n';
-    return 0;
+	go(1, 1 << (Map[0][0] - 'A'), 0, 0);
+	cout << Answer << '\n';
+	return 0;
 }
