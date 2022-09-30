fe2f4971693e1e9e33e2026bcf5b9337902168c7
pushrsp <pushrsp@gmail.com>
2022-09-30 20:55:29 +0900

    [Gold IV] Title: 현욱은 괄호왕이야!!, Time: 8 ms, Memory: 3064 KB -BaekjoonHub

---
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, cnt, ret;
+string str;
+stack<int> s;
+
+int main() {
+  cin >> n;
+  cin >> str; 
+  s.push(-1);
+  for (int i = 0; i < n; i++) {
+    if (str[i] == '(') s.push(i);
+    if (str[i] == ')') {
+      s.pop();
+      if (!s.empty()) {
+      	ret = max(ret, i - s.top()); 
+	  } else { 
+	  	s.push(i);
+	  } 
+  	}
+  }
+  cout << ret << '\n';
+
+  return 0;
+}  
