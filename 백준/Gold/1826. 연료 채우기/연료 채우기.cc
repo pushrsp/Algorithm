0b764383fceb359a0c49aeaaf0bdc408bfad40dd
pushrsp <pushrsp@gmail.com>
2022-10-04 21:59:40 +0900

    [Gold III] Title: 연료 채우기, Time: 4 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.cc
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.cc
@@ -1,30 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <tuple>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int N, L, P;
+vector<pair<int, int>> Stations;
+
+vector<int> get_best(int p, int idx) {
+    vector<int> ret = {-1, -1, -1};
+    for (int i = idx; i < N + 1; ++i) {
+        if (Stations[i].first <= p && ret[1] < Stations[i].second) {
+            ret[0] = Stations[i].first;
+            ret[1] = Stations[i].second;
+            ret[2] = i;
+        }
+    }
+
+    return ret;
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> N;
 
-    vector<pair<int, int>> vec(N);
+    Stations = vector<pair<int, int>>(N);
     for (int n = 0; n < N; ++n)
-        cin >> vec[n].first >> vec[n].second;
+        cin >> Stations[n].first >> Stations[n].second;
 
     cin >> L >> P;
 
-    sort(vec.begin(), vec.end());
+    sort(Stations.begin(), Stations.end());
 
+    int idx = 0, cnt = 0;
     priority_queue<int, vector<int>, less<int>> pq;
-
-    int cnt = 0;
-    int idx = 0;
     while (L > P) {
-        while (vec[idx].first <= P && idx < N) {
-            pq.push(vec[idx].second);
+        while (idx < N && Stations[idx].first <= P) {
+            pq.push(Stations[idx].second);
             idx++;
         }
 
@@ -33,13 +50,14 @@ int main() {
             pq.pop();
             cnt++;
         } else {
-            if (idx == N || vec[idx].first > P) {
-                cnt = -1;
-                break;
-            }
+            break;
         }
     }
 
-    cout << cnt << endl;
+    if (P >= L)
+        cout << cnt << '\n';
+    else
+        cout << -1 << '\n';
+
     return 0;
 }
