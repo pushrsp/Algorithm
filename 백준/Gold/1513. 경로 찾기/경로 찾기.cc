e3a6dcdbd4459e9cd942c1953761d305be02912b
pushrsp <pushrsp@gmail.com>
2022-11-16 11:13:55 +0900

    [Gold II] Title: 경로 찾기, Time: 76 ms, Memory: 28580 KB -BaekjoonHub

--- 백준/Gold/1513. 경로 찾기/경로 찾기.cc
+++ 백준/Gold/1513. 경로 찾기/경로 찾기.cc
@@ -1,38 +1,41 @@
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
 #include <vector>
 #include <cstring>
 
 using namespace std;
 
 const int MOD = 1000007;
+
 int N, M, C, DP[51][51][51][51];
 vector<vector<int>> Map;
 
-int go(int nowY, int nowX, int cnt, int prev) {
-    if (cnt < 0)
+int go(int nowY, int nowX, int count, int visited) {
+    if (count < 0)
         return 0;
-    if (nowY > N || nowX > M)
+    if (nowY > N)
+        return 0;
+    if (nowX > M)
         return 0;
     if (nowY == N && nowX == M) {
-        if (cnt == 0 && Map[nowY][nowX] == 0)
+        if (count == 0 && Map[N][M] == 0)
             return 1;
-        if (cnt == 1 && Map[nowY][nowX] > prev)
+        if (count == 1 && Map[N][M] > visited)
             return 1;
 
         return 0;
     }
 
-    int &ret = DP[nowY][nowX][cnt][prev];
+    int &ret = DP[nowY][nowX][count][visited];
     if (ret != -1)
         return ret;
 
     ret = 0;
     if (Map[nowY][nowX] == 0)
-        ret = (go(nowY + 1, nowX, cnt, prev) + go(nowY, nowX + 1, cnt, prev)) % MOD;
-    else if (Map[nowY][nowX] > prev)
-        ret = (go(nowY + 1, nowX, cnt - 1, Map[nowY][nowX]) + go(nowY, nowX + 1, cnt - 1, Map[nowY][nowX])) % MOD;
-
+        ret = (go(nowY + 1, nowX, count, visited) + go(nowY, nowX + 1, count, visited)) % MOD;
+    else if (Map[nowY][nowX] > visited)
+        ret = (go(nowY + 1, nowX, count - 1, Map[nowY][nowX]) +
+               go(nowY, nowX + 1, count - 1, Map[nowY][nowX])) % MOD;
     return ret;
 }
 
@@ -45,15 +48,15 @@ int main() {
 
     memset(DP, -1, sizeof(DP));
     Map = vector<vector<int>>(N + 1, vector<int>(M + 1, 0));
-
-    for (int i = 1; i <= C; ++i) {
+    for (int c = 1; c <= C; ++c) {
         int y, x;
         cin >> y >> x;
-        Map[y][x] = i;
+
+        Map[y][x] = c;
     }
 
-    for (int i = 0; i <= C; ++i)
-        cout << go(1, 1, i, 0) << ' ';
+    for (int c = 0; c <= C; ++c)
+        cout << go(1, 1, c, 0) << ' ';
 
     return 0;
 }
