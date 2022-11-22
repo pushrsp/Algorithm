46e592ba086f9f411c054c81fa45f69955dd2b31
pushrsp <pushrsp@gmail.com>
2022-11-22 13:50:54 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -1,17 +1,34 @@
 #include <string>
+#include <cstring>
 #include <vector>
-#include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <map>
+#include <sstream>
+#include <vector>
+#include <set>
 #include <cmath>
 
 #define ll long long
 
 using namespace std;
 
-bool is_prime(ll n) {
-    if (n < 2) return false;
+string convert_num(int num, int n) {
+    char code[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    string tmp = "";
+    while (num / n != 0) {
+        tmp = code[num % n] + tmp;
+        num = num / n;
+    }
+    tmp = code[num % n] + tmp;
+    return tmp;
+}
 
-    for (int i = 2; i <= sqrt(n); ++i) {
+bool IsPrime(ll n) {
+    if (n == 0 || n == 1)
+        return false;
+
+    for (ll i = 2; i <= sqrt(n); i++) {
         if (n % i == 0)
             return false;
     }
@@ -19,48 +36,24 @@ bool is_prime(ll n) {
     return true;
 }
 
-string conversion(int n, int k) {
-    string ret = "";
+int solution(int n, int k) {
+    int answer = 0;
+    string num = convert_num(n, k);
+    string temp;
 
-    while (n) {
-        ret += to_string(n % k);
-        n /= k;
-    }
-
-    return string(ret.rbegin(), ret.rend());
-}
-
-vector<ll> split(string &s, char del) {
-    vector<ll> vec;
-    string t;
-
-    for (char i: s) {
-        if (i != del) {
-            t += i;
+    for (char &i: num) {
+        if (i != '0') {
+            temp += i;
         } else {
-            if (t.size() > 0)
-                vec.push_back(atoll(t.c_str()));
+            if (!temp.empty() && IsPrime(stoll(temp)))
+                answer++;
 
-            t = "";
+            temp.clear();
         }
     }
 
-    if (!t.empty())
-        vec.push_back(atoll(t.c_str()));
+    if (!temp.empty() && IsPrime(stoll(temp)))
+        answer++;
 
-    return vec;
-}
-
-int solution(int n, int k) {
-    int answer = 0;
-    string s = conversion(n, k);
-    vector<ll> vec = split(s, '0');
-
-    for (ll v: vec) {
-        if (is_prime(v))
-            answer++;
-    }
-
-    
     return answer;
 }
