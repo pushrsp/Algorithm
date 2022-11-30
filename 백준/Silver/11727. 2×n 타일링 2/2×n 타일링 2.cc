8c625c69a83ce23662512db2dd651c027fa4b979
pushrsp <pushrsp@gmail.com>
2022-11-30 14:53:57 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+const int MOD = 10007;
+int N, DP[1001];
+
+int go(int n) {
+    if (n <= 1)
+        return 1;
+
+    int &ret = DP[n];
+    if (ret)
+        return ret;
+
+    return ret = ((go(n - 1) % MOD) + (go(n - 2) % MOD) + (go(n - 2) % MOD)) % MOD;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N;
+    cout << go(N) << '\n';
+    return 0;
+}
