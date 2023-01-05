e4d25d30597e14356be445b0d19b99e08d15fb20
pushrsp <pushrsp@gmail.com>
2023-01-05 15:41:20 +0900

    [Gold IV] Title: 줄세우기, Time: 0 ms, Memory: 2332 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+#define MAX 40001
+
+using namespace std;
+
+int N, Arr[MAX], DP[MAX];
+
+int BinSearch(int left, int right, int target) {
+    int mid;
+    while (left < right) {
+        mid = (left + right) / 2;
+
+        if (DP[mid] < target)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    return right;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+        cin >> Arr[i];
+
+    DP[0] = Arr[0];
+    int i = 1, j = 0;
+
+    while (i < N) {
+        if (DP[j] < Arr[i]) {
+            DP[j + 1] = Arr[i];
+            j++;
+        } else {
+            int idx = BinSearch(0, j, Arr[i]);
+            DP[idx] = Arr[i];
+        }
+
+        i++;
+    }
+
+    cout << N - (j + 1) << '\n';
+    return 0;
+}
