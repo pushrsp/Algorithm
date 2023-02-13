ccbb08f7c91a817f13749989b865dc112b0dbce8
pushrsp <pushrsp@gmail.com>
2023-02-13 14:32:13 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 12 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.cc
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.cc
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <queue>
-#include <cmath>
+#include <vector>
+#include <algorithm>
 
 #define MAX 100001
 #define ll long long
 
 using namespace std;
 
-ll N, K, Arr[MAX], DP[MAX];
+int N, Arr[MAX];
+ll K;
+ll DP[MAX];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -17,24 +19,18 @@ int main() {
     for (int i = 1; i <= N; ++i)
         cin >> Arr[i];
 
-    int left = 1, right = 1;
-    ll sum = Arr[left];
-    while (left <= N && right <= N) {
-        DP[right] = max(DP[right], DP[right - 1]);
+    int left = 0, right = 1;
+    ll sum = 0;
 
-        if (sum >= K) {
-            DP[right] = max(DP[right], DP[left - 1] + sum - K);
-            if (left < right) {
-                sum -= Arr[left];
-                left++;
-            } else if (left == right) {
-                left++, right++;
-                sum = Arr[left];
-            }
-        } else {
-            right++;
-            sum += Arr[right];
+    while (left <= N && right <= N) {
+        sum += Arr[right];
+        DP[right] = DP[right - 1];
+
+        while (sum >= K) {
+            DP[right] = max(DP[right], DP[left] + sum - K);
+            sum -= Arr[++left];
         }
+        right++;
     }
 
     cout << DP[N] << '\n';
