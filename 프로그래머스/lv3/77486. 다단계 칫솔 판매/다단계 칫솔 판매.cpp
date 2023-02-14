d878053c70482f6322ec91c7a14d62053c25817e
pushrsp <pushrsp@gmail.com>
2023-02-14 15:01:27 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 25.15 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -2,40 +2,48 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <stack>
+#include <queue>
 #include <unordered_map>
+#include <sstream>
+
+#define MAX 10001
 
 using namespace std;
 
 unordered_map<string, string> Tree;
-unordered_map<string, int> Profit;
+unordered_map<string, int> StrToIdx;
+int Amount[MAX];
 
-void go(string &name, int money) {
-    if (name == "center")
+void go(int amount, string referral) {
+    if (referral == "-") {
+        Amount[StrToIdx[referral]] += amount;
         return;
+    }
 
-    int p = static_cast<int>(money * 0.1);
-    Profit.find(name)->second += (money - p);
-
-    if (p < 1)
+    int a = amount / 10;
+    if (a < 1) {
+        Amount[StrToIdx[referral]] += amount;
         return;
+    }
 
-    go(Tree[name], p);
+    Amount[StrToIdx[referral]] += amount - a;
+    go(a, Tree[referral]);
 }
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
-    vector<int> answer;
-
+    StrToIdx["-"] = 0;
     for (int i = 0; i < enroll.size(); ++i) {
-        Tree.insert(make_pair(enroll[i], referral[i] == "-" ? "center" : referral[i]));
-        Profit.insert(make_pair(enroll[i], 0));
+        Tree[enroll[i]] = referral[i];
+        StrToIdx[enroll[i]] = i + 1;
     }
 
-    for (int i = 0; i < seller.size(); ++i)
-        go(seller[i], amount[i] * 100);
+    for (int i = 0; i < seller.size(); ++i) {
+        go(amount[i] * 100, seller[i]);
+    }
 
-    for (auto &e: enroll)
-        answer.push_back(Profit.find(e)->second);
-
+    vector<int> answer;
+    for (int i = 1; i <= enroll.size(); ++i)
+        answer.push_back(Amount[i]);
+    
     return answer;
 }
