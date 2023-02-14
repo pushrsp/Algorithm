48d365d20ace93f1f4289e95cb1828b2a1372f49
pushrsp <pushrsp@gmail.com>
2023-02-14 15:11:37 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,9 +1,8 @@
 select *
 from PLACES
 where HOST_ID in (
-    select HOST_ID
-    from PLACES
+    select HOST_ID 
+    from PLACES 
     group by HOST_ID
-    having count(ID) >= 2
-)
-order by ID asc;
+    having count(HOST_ID) >= 2
+);
