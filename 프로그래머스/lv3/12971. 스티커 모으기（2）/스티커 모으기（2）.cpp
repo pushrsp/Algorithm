06f97c2ce5f83860a1cc7069d508779f04afe2e0
pushrsp <pushrsp@gmail.com>
2023-02-14 18:53:05 +0900

    [level 3] Title: 스티커 모으기(2), Time: 2.65 ms, Memory: 8.57 MB -BaekjoonHub

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -10,26 +10,31 @@
 
 using namespace std;
 
-int DP[MAX], N;
+int DP[MAX], N, Arr[MAX];
+
+int go(int i) {
+    if (i > N)
+        return 0;
+
+    int &ret = DP[i];
+    if (ret != -1)
+        return ret;
+
+    return ret = max(go(i + 2) + Arr[i], go(i + 1));
+}
 
 int solution(vector<int> sticker) {
-    N = static_cast<int>(sticker.size());
-    if (N == 1)
-        return sticker[0];
+    N = static_cast<int>(sticker.size()) - 1;
+    for (int i = 0; i <= N; ++i)
+        Arr[i] = sticker[i];
 
-    int answer = 0;
+    ::memset(DP, -1, sizeof(DP));
 
-    DP[0] = sticker[0], DP[1] = sticker[0];
-    for (int i = 2; i < N - 1; ++i)
-        DP[i] = max(DP[i - 2] + sticker[i], DP[i - 1]);
+    int answer = go(1);
 
-    answer = max(answer, DP[N - 2]);
+    ::memset(DP, -1, sizeof(DP));
+    N = static_cast<int>(sticker.size()) - 2;
 
-    DP[0] = 0, DP[1] = sticker[1];
-    for (int i = 2; i < N; ++i)
-        DP[i] = max(DP[i - 2] + sticker[i], DP[i - 1]);
-
-    answer = max(answer, DP[N - 1]);
-
+    answer = max(answer, go(2) + Arr[0]);
     return answer;
 }
