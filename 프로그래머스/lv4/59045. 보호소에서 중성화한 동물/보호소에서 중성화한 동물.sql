a23550e4287ce4c7c499cbc9616f4b5bc41e66b5
pushrsp <pushrsp@gmail.com>
2023-02-16 18:28:10 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,5 @@
+select ai.ANIMAL_ID as ANIMAL_ID, ai.ANIMAL_TYPE as ANIMAL_TYPE, ai.NAME as NAME
+from ANIMAL_INS ai
+inner join ANIMAL_OUTS ao
+on ai.ANIMAL_ID = ao.ANIMAL_ID
+where ai.SEX_UPON_INTAKE != ao.SEX_UPON_OUTCOME;
