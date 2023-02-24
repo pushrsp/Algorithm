b27ad029032e28f17b308278cdabe144dd2089f0
pushrsp <pushrsp@gmail.com>
2023-02-24 14:50:35 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,103 +1,68 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <cstring>
+#include <algorithm>
+
+#define MAX 17
 
 using namespace std;
 
-int N;
+int N, DP[MAX][MAX][3];
 vector<vector<int>> Map;
 
-int deltaY[3] = {0, 1, 1};
-int deltaX[3] = {1, 1, 0};
-
-struct Pipe {
-    int y, x, type;
-};
-
-/**
- * 0: 가로
- * 1: 세로
- * 2: 대각선
- */
-
-bool check_diagonal(int nowY, int nowX, int destY, int destX) {
-    bool success = true;
-    for (int i = 0; i < 3; ++i) {
-        int nextY = nowY + deltaY[i], nextX = nowX + deltaX[i];
-
-        if (nextY > destY) {
-            success = false;
-            continue;
-        }
-
-        if (nextX > destX) {
-            success = false;
-            continue;
-        }
-
-        if (Map[nextY][nextX])
-            success = false;
-    }
-
-    return success;
+bool InRange(int y, int x) {
+    return 0 <= y && y < N && 0 <= x && x < N;
 }
 
-int bfs(int startY, int startX, int destY, int destX) {
-    queue<Pipe> q;
-    int count = 0;
-
-    q.push({startY, startX, 0});
-    while (!q.empty()) {
-        auto now = q.front();
-        q.pop();
-
-        if (now.y == destY && now.x == destX) {
-            count++;
-            continue;
-        }
-
-        if (now.type == 0) {
-            if (now.x + 1 <= destX && !Map[now.y][now.x + 1])
-                q.push({now.y, now.x + 1, 0});
-
-            if (check_diagonal(now.y, now.x, destY, destX))
-                q.push({now.y + 1, now.x + 1, 2});
-
-        } else if (now.type == 1) {
-            if (now.y + 1 <= destY && !Map[now.y + 1][now.x])
-                q.push({now.y + 1, now.x, 1});
-
-            if (check_diagonal(now.y, now.x, destY, destX))
-                q.push({now.y + 1, now.x + 1, 2});
-        } else {
-            if (now.x + 1 <= destX && !Map[now.y][now.x + 1])
-                q.push({now.y, now.x + 1, 0});
-
-            if (now.y + 1 <= destY && !Map[now.y + 1][now.x])
-                q.push({now.y + 1, now.x, 1});
-
-            if (check_diagonal(now.y, now.x, destY, destX))
-                q.push({now.y + 1, now.x + 1, 2});
-        }
+int go(int y, int x, int type) {
+    if (!InRange(y, x))
+        return 0;
+    if (Map[y][x])
+        return 0;
+    if (type == 2) {
+        if (Map[y - 1][x])
+            return 0;
+        if (Map[y][x - 1])
+            return 0;
     }
 
-    return count;
+    if (y == N - 1 && x == N - 1)
+        return 1;
+
+    int &ret = DP[y][x][type];
+    if (ret != -1)
+        return ret;
+
+    ret = 0;
+    if (type == 0) {
+        ret += go(y, x + 1, 0);
+        ret += go(y + 1, x + 1, 2);
+    } else if (type == 1) {
+        ret += go(y + 1, x, 1);
+        ret += go(y + 1, x + 1, 2);
+    } else {
+        ret += go(y, x + 1, 0);
+        ret += go(y + 1, x, 1);
+        ret += go(y + 1, x + 1, 2);
+    }
+
+    return ret;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL), cout.tie(NULL);
 
     cin >> N;
-    Map = vector<vector<int>>(N, vector<int>(N, 0));
+    Map = vector<vector<int>>(N, vector<int>(N));
 
     for (int y = 0; y < N; ++y) {
-        for (int x = 0; x < N; ++x) {
+        for (int x = 0; x < N; ++x)
             cin >> Map[y][x];
-        }
     }
 
-    cout << bfs(0, 1, N - 1, N - 1) << '\n';
+    ::memset(DP, -1, sizeof(DP));
+
+    cout << go(0, 1, 0) << '\n';
     return 0;
 }
