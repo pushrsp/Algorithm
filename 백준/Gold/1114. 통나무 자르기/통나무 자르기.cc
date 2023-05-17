dddfd5579cef73d06e52d287e5280c008e9a9b0d
pushrsp <pushrsp@gmail.com>
2023-05-17 21:59:45 +0900

    [Gold I] Title: 통나무 자르기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/1114. 통나무 자르기/통나무 자르기.cc
+++ 백준/Gold/1114. 통나무 자르기/통나무 자르기.cc
@@ -1,73 +1,74 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <string>
+#include <set>
+
+#define ll long long
+#define MAX 10001
 
 using namespace std;
 
-int L, K, C;
-int ks[10001];
+ll L, K, C, Pos[MAX];
 
-int check(int tgt) {
+ll Search(ll target) {
+    ll count = C;
+    ll from = L;
 
-    int temp = 0;
-    int from = L;
-    int cut = C;
-    // 오른쪽부터 탐색
-    for(int i = K-1; i>=0 && cut>0; i--) { 
-        if(from-ks[i] > tgt) {  // 나무조각이 목표길이보다 커지면 직전 지점에서 자름
-            if(ks[i+1]-ks[i] > tgt) {  // 목표길이보다 작게 자를수 없는 조각 발견시 -1 반환
+    for (int i = K - 1; i >= 0 && count > 0; --i) {
+        if (from - Pos[i] > target) {
+            if (Pos[i + 1] - Pos[i] > target)
                 return -1;
-            }
-            cut--;
-            from = ks[i+1];
+
+            count--;
+            from = Pos[i + 1];
         }
     }
 
-    if(cut > 0) {
-        from = ks[0];  // 아직 자를 수 있다면 가장 왼쪽에서 자름
-    }
+    if (count > 0)
+        from = Pos[0];
 
-    if(from > tgt) {
-        return -1;  // 남은 조각이 목표길이보다 크면 -1 반환
-    }
-    else {
-        return from; // 그렇지 않으면 자른 위치중 가장 왼쪽값 반환
-    }
+    if (from > target)
+        return -1;
+
+    return from;
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
 
     cin >> L >> K >> C;
-    for(int i = 0; i < K; i++) {
-        cin >> ks[i];
-    }
+    for (int i = 0; i < K; ++i)
+        cin >> Pos[i];
 
-    sort(ks, ks+K);  //자르는 위치 정렬
-    ks[K] = -1;
+    sort(Pos, Pos + K);
+
     int j = 0;
-    for(int i = 0; i < K; i++) {  // 자르는 위치 중복값 제거
-        ks[j] = ks[i];
-        if(ks[i] != ks[i+1]) {
-            j++;
-        }
-    }
-    K = j;
-    ks[K] = L;
+    for (int i = 0; i < K; ++i) {
+        Pos[j] = Pos[i];
 
-    int start = L/(C+1);
-    int end = L;
-    int mid, ret;
-    while(start<end) {  // L/(C+1)부터L까지 이분탐색
-        mid = (start+end)/2;
-        ret = check(mid);
-        if(ret >= 1) {
-            end = mid;
-        }
-        else{
-            start = mid+1;
+        if (Pos[i] != Pos[i + 1])
+            j++;
+    }
+
+    K = j;
+    Pos[K] = L;
+
+    ll left = L / (C + 1), right = L, mid, temp, ret;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        temp = Search(mid);
+
+        if (temp >= 1) {
+            ret = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
-    
-    cout << start << ' ' << check(start) << endl;
-    
+
+    cout << ret << ' ' << Search(ret) << '\n';
+
     return 0;
 }
