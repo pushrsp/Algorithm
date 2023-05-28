b3ee0e4f95d873c118b0d642af8c7666893fc014
pushrsp <pushrsp@gmail.com>
2023-05-28 15:44:28 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1404 ms, Memory: 63120 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
+#include <cmath>
 
 #define MAX 5000001
 
 using namespace std;
 
-int N, L;
-int Arr[MAX];
+int N, L, Arr[MAX];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -21,14 +20,14 @@ int main() {
 
     deque<pair<int, int>> dq;
     for (int i = 1; i <= N; ++i) {
-        if (!dq.empty() && dq.front().second < i - L + 1)
+        while (!dq.empty() && dq.front().first < i - L + 1)
             dq.pop_front();
 
-        while (!dq.empty() && dq.back().first > Arr[i])
+        while (!dq.empty() && dq.back().second > Arr[i])
             dq.pop_back();
-        dq.emplace_back(Arr[i], i);
 
-        cout << dq.front().first << ' ';
+        dq.emplace_back(i, Arr[i]);
+        cout << dq.front().second << ' ';
     }
     return 0;
 }
