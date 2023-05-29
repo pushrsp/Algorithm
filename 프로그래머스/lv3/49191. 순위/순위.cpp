3075a9a15f43a022f285ec16110ede4f3925f943
pushrsp <pushrsp@gmail.com>
2023-05-29 20:27:47 +0900

    [level 3] Title: 순위, Time: 0.91 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.cpp
+++ 프로그래머스/lv3/49191. 순위/순위.cpp
@@ -1,36 +1,39 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
-#include <set>
 
 #define MAX 101
 
 using namespace std;
 
-set<int> Win[MAX], Lose[MAX];
+bool Adj[MAX][MAX];
 
 int solution(int n, vector<vector<int>> results) {
-    for (auto &result: results) {
-        Win[result[1]].insert(result[0]);
-        Lose[result[0]].insert(result[1]);
-    }
+    for (auto &result: results)
+        Adj[result[0]][result[1]] = true;
 
-    for (int i = 1; i <= n; ++i) {
-        for (auto &w: Win[i]) { // i한테 이긴 선수들
-            for (auto &l: Lose[i]) // i한테 진 선수들
-                Lose[w].insert(l);
-        }
-
-        for (auto &l: Lose[i]) { // i한테 진 선수들
-            for (auto &w: Win[i]) // i한테 이긴 선수들
-                Win[l].insert(w);
+    for (int k = 1; k <= n; ++k) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (Adj[i][k] && Adj[k][j])
+                    Adj[i][j] = true;
+            }
         }
     }
 
     int answer = 0;
     for (int i = 1; i <= n; ++i) {
-        if (Win[i].size() + Lose[i].size() == n - 1)
-            answer += 1;
+        int count = 0;
+        for (int j = 1; j <= n; ++j) {
+            if (i == j)
+                continue;
+            if (Adj[i][j] || Adj[j][i])
+                count++;
+        }
+
+        if (count == n - 1)
+            answer++;
     }
 
     return answer;
