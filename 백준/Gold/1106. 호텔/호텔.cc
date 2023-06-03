ac9c0e2dcaa075191cf1d7519fe56386649c1067
pushrsp <pushrsp@gmail.com>
2023-06-03 18:24:21 +0900

    [Gold V] Title: 호텔, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int C, N, DP[100001];
+int C, N, DP[21][1001];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -15,24 +15,29 @@ int main() {
 
     cin >> C >> N;
 
-    vector<pair<int, int>> vec(N);
-    for (int i = 0; i < N; ++i)
+    vector<pair<int, int>> vec(N + 1);
+    for (int i = 1; i <= N; ++i)
         cin >> vec[i].first >> vec[i].second;
 
-    for (int i = 1; i <= 100000; ++i) {
-        for (int j = 0; j < N; ++j) {
-            if (i % vec[j].first == 0)
-                DP[i] = max(DP[i], (i / vec[j].first) * vec[j].second);
+    for (auto &dp: DP)
+        fill(dp, dp + 1001, INF);
 
-            if (i - vec[j].first > 0)
-                DP[i] = max(DP[i], DP[i - vec[j].first] + vec[j].second);
-        }
+    for (int n = 1; n <= N; ++n) {
+        int price = vec[n].first, quantity = vec[n].second;
 
-        if (DP[i] >= C) {
-            cout << i << '\n';
-            return 0;
+        for (int c = 1; c <= C; ++c) {
+            int m = c / quantity, r = c % quantity;
+            if (r > 0)
+                m += 1;
+
+            DP[n][c] = min(m * price, DP[n - 1][c]);
+            if (quantity <= c) {
+                DP[n][c] = min(DP[n][c], DP[n - 1][c - quantity] + price);
+                DP[n][c] = min(DP[n][c], DP[n][c - quantity] + price);
+            }
         }
     }
 
+    cout << DP[N][C] << '\n';
     return 0;
 }
