eb0497e15c3f6fe6f457e8131add1b68930e25bb
pushrsp <pushrsp@gmail.com>
2023-07-04 19:18:30 +0900

    [Gold II] Title: 숫자구슬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2613. 숫자구슬/숫자구슬.cc
+++ 백준/Gold/2613. 숫자구슬/숫자구슬.cc
@@ -1,69 +1,71 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
 using namespace std;
- 
-int N, M;
-int arr[300];
- 
-bool isPossible(int mid) {
-    int sum = 0, groupCnt = 1;
-    for (int i = 0; i < N; i++) {
-        sum += arr[i];
-        if (sum > mid){
-            sum = arr[i];
-            groupCnt++;
-        }
-    }
-    return groupCnt <= M;
-}
- 
-void printAns(int mid){
-    cout << mid << '\n';
- 
-    int i, sum = 0, marbleCnt = 0;
-    for (i=0; i < N; i++) {
-        sum += arr[i];
-        if (sum > mid) {
-            sum = arr[i];
+
+int N, M, Arr[301];
+
+void print(int mx) {
+    cout << mx << '\n';
+
+    int sum = 0, count = 0;
+    for (int i = 0; i < N; ++i) {
+        sum += Arr[i];
+        if (sum > mx) {
+            sum = Arr[i];
             M--;
-            cout << marbleCnt << " ";
-            marbleCnt = 0;
+            cout << count << ' ';
+            count = 0;
         }
-        marbleCnt++;
-        // M개의 그룹을 만들기 위해 최소한의 구슬은 남겨둡니다.
-        // (나머지 그룹에 적어도 구슬 1개를 배치해야 하기 때문.)
-        if (N - i == M) break;
+
+        count++;
+
+        if (N - i == M)
+            break;
     }
- 
-    // 위에서 구한 구슬 개수를 현재 그룹까지만 출력하고
-    // 나머지 남은 그룹에는 구슬 1개씩 배치
-    while (M--){
-        cout << marbleCnt << " ";   
-        marbleCnt = 1;
+
+    while (M--) {
+        cout << count << ' ';
+        count = 1;
     }
 }
- 
- 
+
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
+    cin.tie(NULL), cout.tie(NULL);
+
     cin >> N >> M;
-    int left = 0, right = 0, mid;
-    for(int i=0; i<N; ++i){
-        cin >> arr[i];
-        // 원소 중 최대값으로 left 설정
-        left = left < arr[i] ? arr[i] : left;
-        right += arr[i];
+
+    int left = 0, right = 0, mid, sum, cap, ret;
+    for (int n = 0; n < N; ++n) {
+        cin >> Arr[n];
+        right += Arr[n];
+        left = left < Arr[n] ? Arr[n] : left;
     }
-        
+
     while (left <= right) {
         mid = (left + right) / 2;
-        if(isPossible(mid))
+        sum = 0, cap = 1;
+
+        for (int i = 0; i < N; ++i) {
+            sum += Arr[i];
+            if (sum > mid) {
+                cap += 1;
+                sum = Arr[i];
+            }
+        }
+
+        if (cap <= M) {
+            ret = mid;
             right = mid - 1;
-        else
+        } else {
             left = mid + 1;
+        }
     }
- 
-    // 정답 출력
-    printAns(left);
+
+    print(ret);
+
+    return 0;
 }
