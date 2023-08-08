72cc84e788a463f6f2991e1fd76917082e4c92ed
pushrsp <pushrsp@gmail.com>
2023-08-08 21:09:00 +0900

    [Gold II] Title: 모양 정돈, Time: 8 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Gold/2450. 모양 정돈/모양 정돈.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int N, A[100001], B[100001], Num[100001], Ans = INT32_MAX;
+bool Visited[4];
+vector<int> Temp;
+
+int calc() {
+    int i, j, k = 0, pos[4][4] = {{0}}, cnt1 = 0, cnt2 = 0;
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < Num[Temp[i]]; j++)
+            B[k++] = Temp[i];
+    }
+
+    for (i = 0; i < N; i++)
+        pos[B[i]][A[i]]++;
+
+    for (i = 1; i < 3; i++) {
+        for (j = i + 1; j <= 3; j++) {
+            k = min(pos[i][j], pos[j][i]);
+            cnt1 += k;
+            cnt2 += pos[i][j] + pos[j][i] - (k * 2);
+        }
+    }
+
+    return cnt1 + cnt2 / 3 * 2;
+}
+
+void go(int count) {
+    if (count == 3) {
+        Ans = min(Ans, calc());
+        return;
+    }
+
+    for (int i = 1; i <= 3; i++) {
+        if (Visited[i])
+            continue;
+
+        Visited[i] = true;
+        Temp.push_back(i);
+
+        go(count + 1);
+
+        Visited[i] = false;
+        Temp.pop_back();
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    cin >> N;
+    for (int n = 0; n < N; n++) {
+        cin >> A[n];
+        Num[A[n]]++;
+    }
+
+    go(0);
+
+    cout << Ans << '\n';
+
+    return 0;
+}
