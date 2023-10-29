ff7e15f9217e85df76620bd376b1302acef0c429
pushrsp <pushrsp@gmail.com>
2023-10-29 18:37:13 +0900

    [Gold IV] Title: 자동차경주대회, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2651. 자동차경주대회/자동차경주대회.cc
+++ 백준/Gold/2651. 자동차경주대회/자동차경주대회.cc
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <cstring>
-#include <algorithm>
+#include <vector>
 
 #define INF 1e18
 #define ll long long
 
 using namespace std;
 
-int N;
-ll K, Dist[105], Duration[105], DP[105], Sum[105];
+ll D, S, A[106], C[106], P[106], DP[106];
 vector<int> Nodes;
 
 ll go(int curr) {
-    if (curr >= N + 1)
+    if (curr >= S + 1)
         return 0;
 
     ll &ret = DP[curr];
@@ -20,27 +19,25 @@ ll go(int curr) {
         return ret;
 
     ret = INF;
-    for (int i = curr + 1; i <= N + 1; ++i) {
-        if (Sum[i] - Sum[curr] <= K)
-            ret = min(ret, go(i) + Duration[i]);
+    for (int i = curr + 1; i <= S + 1; ++i) {
+        if (P[i] - P[curr] <= D)
+            ret = min(ret, go(i) + C[i]);
     }
 
     return ret;
 }
 
 void trace(int curr) {
-    if (curr >= N + 1)
+    if (curr >= S + 1)
         return;
 
-    for (int i = curr + 1; i <= N + 1; ++i) {
-        if (Sum[i] - Sum[curr] <= K) {
-            if (DP[curr] - DP[i] == Duration[i]) {
-                if (i != N + 1)
-                    Nodes.push_back(i);
+    for (int i = curr + 1; i <= S + 1; ++i) {
+        if (DP[curr] - DP[i] == C[i]) {
+            if (i != S + 1)
+                Nodes.push_back(i);
 
-                trace(i);
-                break;
-            }
+            trace(i);
+            break;
         }
     }
 }
@@ -49,19 +46,22 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
-    cin >> K;
-    cin >> N;
+    cin >> D;
 
-    for (int n = 1; n <= N + 1; ++n) {
-        cin >> Dist[n];
-        Sum[n] = Sum[n - 1] + Dist[n];
+    cin >> S;
+
+    for (int s = 1; s <= S + 1; ++s) {
+        cin >> A[s];
+        P[s] = P[s - 1] + A[s];
     }
-    for (int n = 1; n <= N; ++n)
-        cin >> Duration[n];
+
+    for (int s = 1; s <= S; ++s)
+        cin >> C[s];
 
     ::memset(DP, -1, sizeof(DP));
 
     ll ret = go(0);
+
     if (ret >= INF) {
         cout << 0 << '\n';
     } else {
@@ -71,9 +71,9 @@ int main() {
         cout << Nodes.size() << '\n';
         for (auto &node: Nodes)
             cout << node << ' ';
+
         cout << '\n';
     }
-
 
     return 0;
 }
