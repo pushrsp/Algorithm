3f50763685f0c8c6637d4778860e62a72f937c99
pushrsp <pushrsp@gmail.com>
2023-11-21 16:30:58 +0900

    [Platinum IV] Title: 영화 수집, Time: 200 ms, Memory: 5536 KB -BaekjoonHub

--- 백준/Platinum/3653. 영화 수집/영화 수집.cc
+++ 백준/Platinum/3653. 영화 수집/영화 수집.cc
@@ -4,15 +4,7 @@
 
 using namespace std;
 
-int T, N, M, SegTree[800001], Arr[200001], Idx[100001];
-
-int init(int node, int start, int end) {
-    if (start == end)
-        return SegTree[node] = Arr[start];
-
-    int mid = (start + end) / 2;
-    return SegTree[node] = init(2 * node, start, mid) + init(2 * node + 1, mid + 1, end);
-}
+int T, N, M, SegTree[800001], Idx[100001];
 
 void update(int node, int start, int end, int idx, int value) {
     if (start <= idx && idx <= end) {
@@ -44,32 +36,21 @@ int main() {
         cin >> N >> M;
 
         ::memset(SegTree, 0, sizeof(SegTree));
-        ::memset(Arr, 0, sizeof(Arr));
         ::memset(Idx, 0, sizeof(Idx));
 
         for (int n = 1; n <= N; ++n) {
-            Arr[n] = 1;
-            Idx[n] = N - (n - 1);
+            Idx[n] = M + n;
+            update(1, 1, N + M, M + n, 1);
         }
 
-        init(1, 1, N + M);
-
-        int dvd;
+        int dvd, next = M;
         for (int m = 1; m <= M; ++m) {
             cin >> dvd;
+            cout << sum(1, 1, N + M, 1, Idx[dvd] - 1) << ' ';
 
-            int curr_idx = Idx[dvd];
-            int next_idx = m + N;
-
-            Arr[curr_idx] = 0;
-            Arr[next_idx] = 1;
-
-            update(1, 1, N + M, curr_idx, -1);
-            update(1, 1, N + M, next_idx, 1);
-
-            Idx[dvd] = next_idx;
-
-            cout << sum(1, 1, N + M, curr_idx, next_idx - 1) << ' ';
+            update(1, 1, N + M, Idx[dvd], -1);
+            Idx[dvd] = next--;
+            update(1, 1, N + M, Idx[dvd], 1);
         }
 
         cout << '\n';
