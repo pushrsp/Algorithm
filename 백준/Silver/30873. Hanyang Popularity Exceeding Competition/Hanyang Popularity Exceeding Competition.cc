534aa69b15f18509e38f8883c57b1c3f8ce9578d
pushrsp <pushrsp@gmail.com>
2024-01-08 11:02:49 +0900

    [Silver V] Title: Hanyang Popularity Exceeding Competition, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/30873. Hanyang Popularity Exceeding Competition/Hanyang Popularity Exceeding Competition.cc
+++ 백준/Silver/30873. Hanyang Popularity Exceeding Competition/Hanyang Popularity Exceeding Competition.cc
@@ -7,26 +7,7 @@
 
 using namespace std;
 
-int N, DP[200001][2];
-vector<pair<int, int>> P;
-
-int go(int n, int meet, int p) {
-    if (n >= N)
-        return 0;
-
-    int &ret = DP[n][meet];
-    if (ret != -1)
-        return ret;
-
-    ret = 0;
-
-    if (abs(P[n].first - p) <= P[n].second) //현재 유명인을 만날 수 있는지
-        ret = max(ret, go(n + 1, 1, p + 1) + 1); //현재 유명인을 만날건지
-
-    ret = max(ret, go(n + 1, 0, p)); //현재 유명인을 스킵할건지
-
-    return ret;
-}
+int N;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -34,15 +15,15 @@ int main() {
 
     cin >> N;
 
-    int p, c;
+    int p, c, ret = 0;
     for (int n = 0; n < N; ++n) {
         cin >> p >> c;
-        P.emplace_back(p, c);
+
+        if (abs(p - ret) <= c)
+            ret += 1;
     }
 
-    ::memset(DP, -1, sizeof(DP));
-
-    cout << go(0, 0, 0) << '\n';
+    cout << ret << '\n';
 
     return 0;
 }
