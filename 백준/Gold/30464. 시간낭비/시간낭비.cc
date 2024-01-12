d00c7a3a049ce8849642cb7963df0cd38e1892e7
pushrsp <pushrsp@gmail.com>
2024-01-12 17:30:46 +0900

    [Gold II] Title: 시간낭비, Time: 36 ms, Memory: 35488 KB -BaekjoonHub

--- 백준/Gold/30464. 시간낭비/시간낭비.cc
+++ 백준/Gold/30464. 시간낭비/시간낭비.cc
@@ -1,28 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <set>
 #include <cstring>
-#include <algorithm>
+
+#define INF -987654321
 
 using namespace std;
 
-int N, Arr[200001], DP[200001][2][3];
+int N, A[200001], DP[200001][3][2];
 
-int go(int n, int m, int o) {
+/*
+ * d 0: ->
+ * d 1: <-
+ */
+int go(int n, int c, int d) {
     if (n < 1 || n > N)
-        return -1e9;
+        return INF;
+
     if (n == N)
         return 0;
 
-    int &ret = DP[n][m][o];
+    int &ret = DP[n][c][d];
     if (ret != -1)
         return ret;
 
-    ret = -1e9;
+    ret = INF;
 
-    int dist = Arr[n] * (m ? -1 : 1);
+    int nextN = A[n] * (d ? -1 : 1);
+    ret = max(ret, go(n + nextN, c, d) + 1);
 
-    ret = go(n + dist, m, o) + 1;
-    if (o < 2)
-        ret = max(ret, go(n - dist, (m + 1) % 2, o + 1) + 1);
+    if (c > 0)
+        ret = max(ret, go(n - nextN, c - 1, (d + 1) % 2) + 1);
 
     return ret;
 }
@@ -34,11 +42,12 @@ int main() {
     cin >> N;
 
     for (int n = 1; n <= N; ++n)
-        cin >> Arr[n];
+        cin >> A[n];
 
-    ::memset(DP, -1, sizeof(DP));
+    fill(&DP[0][0][0], &DP[0][0][0] + 200001 * 3 * 2, -1);
 
-    int ret = go(1, 0, 0);
+    int ret = go(1, 2, 0);
+
     if (ret > 0)
         cout << ret << '\n';
     else
