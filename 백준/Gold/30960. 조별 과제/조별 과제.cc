15c6f834fd0f677ce50e0703ed0533ce7fbf3975
pushrsp <pushrsp@gmail.com>
2024-02-15 12:38:18 +0900

    [Gold V] Title: 조별 과제, Time: 92 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/30960. 조별 과제/조별 과제.cc
+++ 백준/Gold/30960. 조별 과제/조별 과제.cc
@@ -1,16 +1,15 @@
-#include <iostream>
+#include <cstring>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <cmath>
+#include <queue>
 
 #define ll long long
 
 using namespace std;
 
 int N, A[500001];
-
-bool asc(const pair<int, pair<int, int>> &a, const pair<int, pair<int, int>> &b) {
-    return a.first < b.first;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -21,10 +20,10 @@ int main() {
     for (int n = 1; n <= N; ++n)
         cin >> A[n];
 
-    sort(A, A + N + 1);
+    sort(A + 1, A + 1 + N);
 
     ll x = 0, y = INT64_MAX;
-    for (int n = 2; n <= N; n += 2) {
+    for (int n = 2; n <= N - 1; n += 2) {
         x += A[n] - A[n - 1];
         y = min(y, x) + A[n + 1] - A[n];
     }
