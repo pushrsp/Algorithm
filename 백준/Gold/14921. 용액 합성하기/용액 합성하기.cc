c0ecc1dde083cd1ef72ce20c3fda79638977487b
pushrsp <pushrsp@gmail.com>
2024-03-23 11:06:34 +0900

    [Gold V] Title: 용액 합성하기, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -12,62 +12,28 @@ using namespace std;
 
 int N;
 
-ll get_diff(ll a, ll b) {
-    return abs(a + b);
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
 
     cin >> N;
 
-    map<ll, int> m;
     vector<ll> v(N);
-    for (int n = 0; n < N; ++n) {
+    for (int n = 0; n < N; ++n)
         cin >> v[n];
-        m[v[n]]++;
-    }
 
     ll ret = INT64_MAX, offset = INT64_MAX;
-    for (auto &n: v) {
-        int i = lower_bound(v.begin(), v.end(), -n) - v.begin();
-        if (i == N)
-            i--;
-
-        if (n == v[i]) {
-            if (m[n] > 1) {
-                ll diff = get_diff(n, v[i]);
-                if (diff < offset) {
-                    offset = diff;
-                    ret = n + v[i];
-                }
-            }
-        } else {
-            ll diff = get_diff(n, v[i]);
-            if (diff < offset) {
-                offset = diff;
-                ret = n + v[i];
-            }
+    int i = 0, j = N - 1;
+    while (i < j) {
+        if (offset > abs(v[i] + v[j])) {
+            offset = abs(v[i] + v[j]);
+            ret = v[i] + v[j];
         }
 
-        if (i != 0) {
-            if (n == v[i - 1]) {
-                if (m[n] > 1) {
-                    ll diff = get_diff(n, v[i - 1]);
-                    if (diff < offset) {
-                        offset = diff;
-                        ret = n + v[i - 1];
-                    }
-                }
-            } else {
-                ll diff = get_diff(n, v[i - 1]);
-                if (diff < offset) {
-                    offset = diff;
-                    ret = n + v[i - 1];
-                }
-            }
-        }
+        if (v[i] + v[j] < 0)
+            i++;
+        else
+            j--;
     }
 
     cout << ret << '\n';
