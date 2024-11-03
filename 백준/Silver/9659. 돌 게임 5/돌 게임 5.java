593d5aee43032ab1ccbe9c48fa2b047c4994686c
pushrsp <pushrsp@gmail.com>
2024-11-03 18:41:18 +0900

    [Silver III] Title: 돌 게임 5, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static long N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Long.parseLong(st.nextToken());
+
+        if (N % 2 == 0) {
+            bw.write("CY\n");
+        } else {
+            bw.write("SK\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
