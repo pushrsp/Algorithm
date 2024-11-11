ef03bf815fc237ce454224cfc96ca8250bc9ba10
pushrsp <pushrsp@gmail.com>
2024-11-11 21:27:41 +0900

    [Gold V] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Easy), Time: 540 ms, Memory: 20484 KB -BaekjoonHub

--- 백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.java
+++ 백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.java
@@ -4,6 +4,23 @@ import java.util.StringTokenizer;
 public class Main {
 
     private static int K;
+    private static boolean[] PRIME = new boolean[1000001];
+
+    private static int go(int k) {
+        if (PRIME[k]) {
+            return 1;
+        }
+
+        int ret = Integer.MAX_VALUE;
+        for (int i = 2; i * i <= k; i++) {
+            if (k % i == 0) {
+                int left = go(i) + 1;
+                int right = go(k / i) + 1;
+                ret = Math.min(ret, Math.max(left, right));
+            }
+        }
+        return ret;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,25 +28,20 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         K = Integer.parseInt(st.nextToken());
+        for (int k = 2; k <= K; k++) {
+            PRIME[k] = true;
+        }
 
-        int ret = 1;
-        while (true) {
-            boolean isPrime = true;
-            for (int k = 2; k < K; k++) {
-                if (K % k == 0) {
-                    isPrime = false;
-                    K /= k;
-                    ret++;
-                    break;
+        for (int i = 2; i * i <= K; i++) {
+            if (PRIME[i]) {
+                for (int j = i * i; j <= K; j += i) {
+                    PRIME[j] = false;
                 }
             }
-
-            if (isPrime) {
-                break;
-            }
         }
-        
-        bw.write(String.valueOf((int) Math.ceil(Math.log10(ret) / Math.log10(2))) + '\n');
+
+        bw.write(String.valueOf(go(K) - 1) + '\n');
+
         br.close();
         bw.close();
     }
