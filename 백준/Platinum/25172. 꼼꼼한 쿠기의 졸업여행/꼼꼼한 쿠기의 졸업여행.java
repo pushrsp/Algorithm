3db5526d542bd1b3d69464eca295ff28eabc8315
pushrsp <pushrsp@gmail.com>
2024-11-16 17:50:23 +0900

    [Platinum IV] Title: 꼼꼼한 쿠기의 졸업여행, Time: 888 ms, Memory: 98724 KB -BaekjoonHub

--- 백준/Platinum/25172. 꼼꼼한 쿠기의 졸업여행/꼼꼼한 쿠기의 졸업여행.java
+++ 백준/Platinum/25172. 꼼꼼한 쿠기의 졸업여행/꼼꼼한 쿠기의 졸업여행.java
@@ -7,24 +7,25 @@ import java.util.StringTokenizer;
 public class Main {
 
     private static int N, M;
-    private static List<List<Integer>> G = new ArrayList<>();
+    private static boolean[] V;
     private static int[] P;
+    private static List<List<Integer>> G = new ArrayList<>();
 
-    private static int getParent(int node) {
-        if (P[node] == node) {
-            return node;
+    private static int getParent(int n) {
+        if (P[n] == n) {
+            return n;
         }
-        return P[node] = getParent(P[node]);
+        return P[n] = getParent(P[n]);
     }
 
-    private static void union(int n1, int n2) {
-        n1 = getParent(n1);
-        n2 = getParent(n2);
+    private static void union(int a, int b) {
+        a = getParent(a);
+        b = getParent(b);
 
-        if (n1 < n2) {
-            P[n1] = n2;
+        if (a < b) {
+            P[b] = a;
         } else {
-            P[n2] = n1;
+            P[a] = b;
         }
     }
 
@@ -35,8 +36,9 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        P = new int[N + 1];
 
+        V = new boolean[N + 1];
+        P = new int[N + 1];
         for (int n = 0; n <= N; n++) {
             G.add(new ArrayList<>());
             P[n] = n;
@@ -52,43 +54,36 @@ public class Main {
             G.get(b).add(a);
         }
 
-        // 완성된 그래프에서 노드를 삭제하는 방식은 시간 복잡도가 너무 커서
-        // 오프라인 쿼리를 통해 빈 공간에 노드를 추가해 그래프를 만드는 방식을 채택
-        final Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         for (int n = 0; n < N; n++) {
-            st = new StringTokenizer(br.readLine());
-            stack.add(Integer.parseInt(st.nextToken()));
+            stack.push(Integer.parseInt(br.readLine()));
         }
 
-        final Stack<Boolean> ret = new Stack<>();
-        ret.add(false); // 마지막은 무조건 false
+        Stack<Boolean> ans = new Stack<>();
+        ans.add(false);
 
-        final boolean[] visited = new boolean[N + 1];
-        int group = 0;
+        int component = 0;
         while (!stack.isEmpty()) {
-            final int node = stack.pop();
-            visited[node] = true;
-            group++;
+            int curr = stack.pop();
+            V[curr] = true;
+            component++;
 
-            // 추가하려는 노드가 연결된 노드 즉 자식 노드일 경우
-            // 한 그룹에 속하므로 group--
-            for (int child : G.get(node)) {
-                if (!visited[child]) {
+            for (int child : G.get(curr)) {
+                if (!V[child]) {
                     continue;
                 }
 
-                if (getParent(node) != getParent(child)) {
-                    union(node, child);
-                    group--;
+                if (getParent(curr) != getParent(child)) {
+                    union(curr, child);
+                    component--;
                 }
             }
-
-            ret.add(group == 1);
+            ans.add(component == 1);
         }
 
-        while (!ret.isEmpty()) {
-            final boolean ans = ret.pop();
-            if (ans) {
+        while (!ans.isEmpty()) {
+            boolean ret = ans.pop();
+            if (ret) {
                 bw.write("CONNECT\n");
             } else {
                 bw.write("DISCONNECT\n");
