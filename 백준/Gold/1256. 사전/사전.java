7eac8cdbbcbe5011e73d4f7f360ad035d32ca549
pushrsp <pushrsp@gmail.com>
2024-11-17 21:24:47 +0900

    [Gold II] Title: 사전, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/1256. 사전/사전.java
+++ 백준/Gold/1256. 사전/사전.java
@@ -1,37 +1,14 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     private static int N, M;
     private static long K;
-    private static long[][] DP = new long[501][501];
+    private static long[][] DP = new long[101][101];
 
-    private static long comb(int s, int r) {
-        if (s == r || r <= 0) {
-            return 1;
-        }
-        if (DP[s][r] != -1) {
-            return DP[s][r];
-        }
-        long v1 = comb(s - 1, r - 1);
-        long v2 = comb(s - 1, r);
-        if (v1 + v2 > 1_000_000_000L) {
-            return DP[s][r] = 0x3f3f3f3f3f3f3f3fL;
-        }
-        return DP[s][r] = v1 + v2;
-    }
+    private static void go(int a, int z) {
 
-    private static void go(int n, int m, long k, BufferedWriter bw) throws IOException {
-        long total = comb(n + m - 1, n - 1);
-        if (n > 0 && k <= total) {
-            bw.write('a');
-            go(n - 1, m, k, bw);
-        } else if (m > 0) {
-            bw.write('z');
-            go(n, m - 1, k - total, bw);
-        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -43,16 +20,50 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         K = Long.parseLong(st.nextToken());
 
-        for (int i = 0; i < 501; i++) {
-            Arrays.fill(DP[i], -1);
+        for (int i = 1; i <= 100; i++) {
+            DP[i][0] = DP[0][i] = 1;
         }
 
-        if (comb(N + M, N) < K) {
+        for (int n = 1; n <= N; n++) {
+            for (int m = 1; m <= M; m++) {
+                DP[n][m] = DP[n - 1][m] + DP[n][m - 1];
+                if (DP[n][m] > 1000000000) {
+                    DP[n][m] = 1000000000;
+                }
+            }
+        }
+
+        if (DP[N][M] < K) {
             bw.write(String.valueOf(-1) + '\n');
         } else {
-            go(N, M, K, bw);
-            bw.write('\n');
+            int aCount = N;
+            int zCount = M;
+
+            for (int i = 0; i < N + M; i++) {
+                if (aCount == 0) {
+                    bw.write('z');
+                    zCount--;
+                    continue;
+                }
+
+                if (zCount == 0) {
+                    bw.write('a');
+                    aCount--;
+                    continue;
+                }
+
+                long count = DP[aCount - 1][zCount];
+                if (K <= count) {
+                    bw.write('a');
+                    aCount--;
+                } else {
+                    K -= count;
+                    bw.write('z');
+                    zCount--;
+                }
+            }
         }
+        bw.write('\n');
 
         br.close();
         bw.close();
