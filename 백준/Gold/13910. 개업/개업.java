e4ed6be023fc9a8285a36a40150baa2ff7573d83
pushrsp <pushrsp@gmail.com>
2024-12-02 21:40:34 +0900

    [Gold V] Title: 개업, Time: 268 ms, Memory: 20400 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.java
+++ 백준/Gold/13910. 개업/개업.java
@@ -7,58 +7,49 @@ import java.util.StringTokenizer;
 public class Main {
 
     private static int N, M;
-    private static int[] DP = new int[10001];
+    private static int[] DP = new int[20001];
     private static List<Integer> W = new ArrayList<>();
-
-    private static int go(int remain) {
-        if (remain == 0) {
-            return 0;
-        }
-
-        if (DP[remain] != -1) {
-            return DP[remain];
-        }
-
-        DP[remain] = 9999999;
-        for (int l = 0; l < M + 1; l++) {
-            for (int r = 0; r < M + 1; r++) {
-                if (l == r) {
-                    continue;
-                }
-
-                int sum = W.get(l) + W.get(r);
-                if (sum > remain) {
-                    continue;
-                }
-
-                DP[remain] = Math.min(DP[remain], go(remain - sum) + 1);
-            }
-        }
-        return DP[remain];
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Arrays.fill(DP, -1);
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        W.add(0);
+        Arrays.fill(DP, 9999999);
 
-        for (int m = 0; m < M; m++) {
+        st = new StringTokenizer(br.readLine());
+
+        W.add(0);
+        for (int m = 1; m <= M; m++) {
             W.add(Integer.parseInt(st.nextToken()));
         }
 
-        int ret = go(N);
-        if (ret == 9999999) {
+        for (int i = 0; i < W.size(); i++) {
+            for (int j = i + 1; j < W.size(); j++) {
+                DP[W.get(i) + W.get(j)] = 1;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (DP[i] != 9999999) {
+                continue;
+            }
+
+            for (int j = 1; j < i; j++) {
+                if (DP[j] == 9999999 || DP[i - j] == 9999999) {
+                    continue;
+                }
+                DP[i] = Math.min(DP[i], DP[j] + DP[i - j]);
+            }
+        }
+
+        if (DP[N] == 9999999) {
             bw.write(String.valueOf(-1) + '\n');
         } else {
-            bw.write(String.valueOf(ret) + '\n');
+            bw.write(String.valueOf(DP[N]) + '\n');
         }
 
         br.close();
