3d0cb88af5621198a916b0a3c8ddc305ce783e3d
pushrsp <pushrsp@gmail.com>
2025-11-14 16:54:12 +0900

    [Silver I] Title: 개미, Time: 424 ms, Memory: 56340 KB -BaekjoonHub

--- 백준/Silver/4307. 개미/개미.java
+++ 백준/Silver/4307. 개미/개미.java
@@ -1,47 +1,37 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int T;
 
-    public static void main(String[] args) throws Exception {
+    private static int T, L, N;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         T = Integer.parseInt(st.nextToken());
-
-        int s, n;
         while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
-            s = Integer.parseInt(st.nextToken());
-            n = Integer.parseInt(st.nextToken());
 
-            List<Integer> pos = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
+            L = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+
+            List<Integer> ants = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                pos.add(Integer.parseInt(st.nextToken()));
+                ants.add(Integer.parseInt(st.nextToken()));
             }
 
-            int shortest = 0, longest = 0;
-            for (Integer p : pos) {
-                if (Math.min(p, s - p) - shortest > 0) {
-                    shortest += Math.min(p, s - p) - shortest;
-                }
-                if (Math.max(p, s - p) - longest > 0) {
-                    longest += Math.max(p, s - p) - longest;
-                }
+            int min = 0, max = 0;
+            for (Integer ant : ants) {
+                min = Math.max(min, Math.min(ant, L - ant));
+                max = Math.max(max, Math.max(ant, L - ant));
             }
 
-            Collections.sort(pos);
-
-            bw.write(String.valueOf(shortest) + ' ' + String.valueOf(longest) + '\n');
+            bw.write(String.valueOf(min) + ' ' + max + '\n');
         }
 
         br.close();
