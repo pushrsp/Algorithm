c134cb280bd39da6c994c0c12fc7a9cd5333c3a7
pushrsp <pushrsp@gmail.com>
2026-01-02 14:50:46 +0900

    [Gold II] Title: 쿠의 용돈 인상 계획, Time: 3904 ms, Memory: 36192 KB -BaekjoonHub

--- 백준/Gold/34766. 쿠의 용돈 인상 계획/쿠의 용돈 인상 계획.java
+++ 백준/Gold/34766. 쿠의 용돈 인상 계획/쿠의 용돈 인상 계획.java
@@ -3,84 +3,67 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    private static int T, D, K;
+    private static String N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int T = Integer.parseInt(br.readLine().trim());
+        T = Integer.parseInt(st.nextToken());
         while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int D = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            String N = br.readLine();
+            st = new StringTokenizer(br.readLine());
+            D = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            N = br.readLine();
 
-            // 가장 뒤에 있는 >= 2인 숫자의 위치 찾기
-            int latestIndex = -1;
+            int latestIndex = 0;
             for (int i = 0; i < D; i++) {
-                if ((N.charAt(i) - '0') >= 2) {
+                if (((int) N.charAt(i) - '0') * 9 > 10) {
                     latestIndex = i;
                 }
             }
 
-            StringBuilder left, right;
-            if (latestIndex == -1) {
-                // 모두 0 또는 1인 경우
-                left = new StringBuilder();
-                right = new StringBuilder(N);
-            } else {
-                left = new StringBuilder(N.substring(0, latestIndex + 1));
-                right = new StringBuilder(N.substring(latestIndex + 1));
-            }
+            StringBuilder left = new StringBuilder(N.substring(0, latestIndex + 1));
+            StringBuilder right = new StringBuilder(N.substring(latestIndex + 1));
 
             while (K-- > 0) {
-                // 가장 뒤쪽의 >= 2인 숫자를 찾아서 9배
-                int idx = -1;
-
-                // right에서 뒤에서부터 찾기
+                int index = -1;
                 for (int i = right.length() - 1; i >= 0; i--) {
-                    if ((right.charAt(i) - '0') >= 2) {
-                        idx = i;
+                    if (right.charAt(i) - '0' > 1) {
+                        index = i;
                         break;
                     }
                 }
 
-                if (idx != -1) {
-                    // right에서 찾음
-                    int val = (right.charAt(idx) - '0') * 9;
-                    if (val >= 10) {
-                        right.setCharAt(idx, (char) ('0' + val / 10));
-                        right.insert(idx + 1, (char) ('0' + val % 10));
-                    } else {
-                        right.setCharAt(idx, (char) ('0' + val));
-                    }
+                if (index != -1) {
+                    int result = ((int) right.charAt(index) - '0') * 9;
+                    right.setCharAt(index, String.valueOf(result).charAt(0));
+                    right.insert(index + 1, String.valueOf(result).charAt(1));
                 } else {
-                    // left에서 뒤에서부터 찾기
                     for (int i = left.length() - 1; i >= 0; i--) {
-                        if ((left.charAt(i) - '0') >= 2) {
-                            idx = i;
+                        if (left.charAt(i) - '0' > 1) {
+                            index = i;
                             break;
                         }
                     }
 
-                    if (idx != -1) {
-                        // left에서 찾음
-                        int val = (left.charAt(idx) - '0') * 9;
-                        if (val >= 10) {
-                            left.setCharAt(idx, (char) ('0' + val / 10));
-                            left.insert(idx + 1, (char) ('0' + val % 10));
-                        } else {
-                            left.setCharAt(idx, (char) ('0' + val));
-                        }
+                    if (index != -1) {
+                        int result = ((int) left.charAt(index) - '0') * 9;
+                        left.setCharAt(index, String.valueOf(result).charAt(0));
+                        left.insert(index + 1, String.valueOf(result).charAt(1));
                     } else {
-                        // 모두 0 또는 1, 맨 앞의 1을 9배
                         for (int i = 0; i < left.length(); i++) {
                             if (left.charAt(i) == '1') {
-                                left.setCharAt(i, '9');
+                                index = i;
                                 break;
                             }
                         }
-                        // left에 없으면 right에서 찾기
-                        if (idx == -1) {
+
+                        if (index != -1) {
+                            left.setCharAt(index, '9');
+                        } else {
                             for (int i = 0; i < right.length(); i++) {
                                 if (right.charAt(i) == '1') {
                                     right.setCharAt(i, '9');
@@ -92,10 +75,9 @@ public class Main {
                 }
             }
 
-            bw.write(left.toString() + right.toString() + '\n');
+            bw.write(left + right.toString() + '\n');
         }
 
-        bw.flush();
         br.close();
         bw.close();
     }
